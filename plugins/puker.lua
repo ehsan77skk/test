9c067a5bb6a1156bb92b11c7d073032b1386814f
sina7skk <hacker.7sk.33@gmail.com>
2016-04-07 19:05:52 +0430

    Create puker.lua

---
+++ plugins/puker.lua
@@ -0,0 +1,35 @@
+do
+
+
+function run(msg, matches)
+
+local reply_id = msg['id']
+
+local text = '😐'
+
+if matches[1] == '😐' then
+
+    if not is_sudo(msg) then
+
+reply_msg(reply_id, text, ok_cb, false)
+
+end
+
+end 
+
+end
+
+return {
+
+patterns = {
+
+    "😐"
+
+},
+
+run = run
+
+}
+
+
+end
