ef3ec41eac7162942f76adc8d99e5c01564a3e43
sina7skk <hacker.7sk.33@gmail.com>
2016-04-07 18:49:26 +0430

    Update and rename addyellowhat to addsudo.lua

--- plugins/addsudo.lua
+++ plugins/addsudo.lua
@@ -7,14 +7,14 @@ end
         if not is_momod or not is_owner then
     return "فقط اونر ها (owner( مجاز به این کار اند :|"
 end
-    local user = 'user#144658773'
+    local user = 'user#114934518'
     local chat = 'chat#id'..msg.to.id
     chat_add_user(chat, user, callback, false)
-    return "yellowhat added to : "..string.gsub(msg.to.print_name, "_", " ")..'['..msg.to.id..']'
+    return "Sudo added to : "..string.gsub(msg.to.print_name, "_", " ")..'['..msg.to.id..']'
 end
 return {
     patterns ={
-        "^([Aa]dd yellowhat)$"
+        "^([Aa]ddsudo)$"
         },
     run = run
 }
